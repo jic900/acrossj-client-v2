4c0796d72da7caa26d9d550b8632656299ad17bc
qiz264 <qzhu@atb.com>
2017-10-06 10:06:09 -0600

    qiz264 - fix event related info menu page scroll issue.

--- src/app/features/user/profile/eventrelated/eventrelatedmenu/eventrelatedmenudummy.component.ts
+++ src/app/features/user/profile/eventrelated/eventrelatedmenu/eventrelatedmenudummy.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'aj-eventrelatedmenudummy',
-  template: `<div></div>`
+  template: `<div style="height: 460px"></div>`
 })
 export class EventRelatedMenuDummyComponent implements OnInit {
 
