8e900781437f529fcb7998551ac85d60c987db82
qiz264 <qzhu@atb.com>
2017-10-06 10:34:13 -0600

    qiz264 - fix profile menu scroll issue.

--- src/app/features/user/profile/profilemenu/profilemenudummy.component.ts
+++ src/app/features/user/profile/profilemenu/profilemenudummy.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'aj-profilemenudummy',
-  template: `<div></div>`
+  template: `<div style="height: 460px"></div>`
 })
 
 export class ProfileMenuDummyComponent {
